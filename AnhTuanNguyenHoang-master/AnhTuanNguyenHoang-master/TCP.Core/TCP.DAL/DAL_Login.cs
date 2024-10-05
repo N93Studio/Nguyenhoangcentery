c4834122fc8c0922c502fe206cf08b623ef1f03b
N93Studio <n93ftp@gmail.com>
2024-10-05 11:48:36 +0700

    v1

---
+++ AnhTuanNguyenHoang-master/AnhTuanNguyenHoang-master/TCP.Core/TCP.DAL/DAL_Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using TCP.DTO;
+
+namespace TCP.DAL
+{
+    public class DAL_Login
+    {
+        public IEnumerable<DTO_Login> TCP_Logincheck(string UserName, string Password)
+        {
+            try
+            {
+                var param = new DynamicParameters();
+                param.Add("@UserName", UserName);
+                param.Add("@Password", Password);
+                return DapperHelper.Query<DTO_Login>("TCP_Logincheck", param);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+        public IEnumerable<DTO_Login> GETALL(int Id)
+        {
+            try
+            {
+                var param = new DynamicParameters();
+                param.Add("@Id", Id);
+                return DapperHelper.Query<DTO_Login>("TCP_Login_SEL", param);
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+    }
+}
